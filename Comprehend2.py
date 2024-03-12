043139863cdcd8ba91cae8489a259c53bf1987ad
pol-gb <114195736+pol-gb@users.noreply.github.com>
2024-03-12 22:05:30 +0100

    Update

--- Comprehend2.py
+++ Comprehend2.py
@@ -5,13 +5,16 @@ from pathlib import Path
 import boto3
 
 if __name__ == "__main__":
-    parser = ArgumentParser(prog="Faces summary", description="")
+    parser = ArgumentParser(
+        prog="Faces summary",
+        description="Search for images where a face appears and do a summary of their attributes",
+    )
     parser.add_argument("face_image", help="An image with the face to search")
     parser.add_argument(
         "--similarity_threshold",
         default=80,
         type=float,
-        help="An image with the face to search",
+        help="Face image similarity threshold",
     )
     parser.add_argument("directory", help="The images directory to analyze")
     args = parser.parse_args()
@@ -21,6 +24,8 @@ if __name__ == "__main__":
     input_image = open(args.face_image, "rb").read()
     images_dir = Path(args.directory)
     for image_file in images_dir.iterdir():
+        print()
+
         with open(image_file, "rb") as directory_image:
             dir_image_bytes = directory_image.read()
             response_match = client.compare_faces(
@@ -32,7 +37,16 @@ if __name__ == "__main__":
             if response_match["FaceMatches"]:
                 print(f"Face match in {image_file}")
 
-                response = client.detect_faces(Image={"Bytes": dir_image_bytes}, Attributes=["GENDER", "EYEGLASSES", "SUNGLASSES", "AGE_RANGE", "SMILE"])
+                response = client.detect_faces(
+                    Image={"Bytes": dir_image_bytes},
+                    Attributes=[
+                        "GENDER",
+                        "EYEGLASSES",
+                        "SUNGLASSES",
+                        "AGE_RANGE",
+                        "SMILE",
+                    ],
+                )
 
                 print(f"Detected faces for {image_file}")
                 for faceDetail in response["FaceDetails"]:
@@ -43,13 +57,17 @@ if __name__ == "__main__":
                         + str(faceDetail["AgeRange"]["High"])
                         + " years old"
                     )
-
-                    # Access predictions for individual face details and print them
                     if faceDetail["Gender"]["Confidence"] > 60:
                         print(f"The detected gender is {faceDetail['Gender']['Value']}")
-                    if faceDetail["Smile"]["Confidence"] > 60 and faceDetail["Smile"]["Value"]:
+                    if (
+                        faceDetail["Smile"]["Confidence"] > 60
+                        and faceDetail["Smile"]["Value"]
+                    ):
                         print("The person is smiling")
-                    if faceDetail["Eyeglasses"]["Confidence"] > 60 and faceDetail["Eyeglasses"]["Value"]:
+                    if (
+                        faceDetail["Eyeglasses"]["Confidence"] > 60
+                        and faceDetail["Eyeglasses"]["Value"]
+                    ):
                         print("And its wearing eyeglasses")
             else:
                 print(f"No match in {image_file}")
